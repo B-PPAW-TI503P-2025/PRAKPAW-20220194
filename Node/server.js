7aed2b2050c8adb6392dc5e6e49a139a41e6e3bb
“DhandhaDendriyaE” <dhanda.dendriya.ft22@mail.umy.ac.id>
2025-11-13 19:15:28 +0700

    lalu menambahkan route baru di server.js

--- Node/server.js
+++ Node/server.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
+
 app.get("/", (req, res) => {
   res.send("Home Page for API");
 });
